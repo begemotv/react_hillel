224b47cabdcc9f7e37896c9d7be483bb933f67e4
begemotv <62621854+begemotv@users.noreply.github.com>
2020-12-13 15:27:20 +0200

    структура компонентов

--- src/App.js
+++ src/App.js
@@ -1,28 +1,12 @@
 import React from "react";
 
-import Button from "./components/button/button";
-import Notification from "./components/notification/notification";
-import ToolTip from "./components/tool-tip/tool-tip";
+import SearchResults from "./components/search-results/search-results";
 
 function App() {
   return (
     <React.Fragment>
-      <div style={{display: `flex`}}>
-        <Button buttonState="default" />
-        <Button buttonState="error" />
-        <Button buttonState="success" />
-      </div>
-
-      <div>
-        <Notification header="Against all odds" text="As am hastily invited settled at limited civilly fortune me. Really spring in extent an by. Judge but built gay party world." notificationState="default" />
-        <Notification header="Always add value" text="He moonlight difficult engrossed an it sportsmen. Interested has all devonshire difficulty gay assistance joy." notificationState="error" />
-        <Notification header="Action gets results" text="Nor hence hoped her after other known defer his. For county now sister engage had season better had waited." notificationState="success" />
-      </div>
-      
-      <div>
-        <ToolTip />
-      </div>
-   </React.Fragment>
+      <SearchResults />
+    </React.Fragment>
   );
 }
 
---
+++ src/components/search-map/search-map.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import './search-map.css';
+
+const SearchMap = () => {
+    return (
+        <React.Fragment>
+        </React.Fragment>
+    )
+}
+
+export default SearchMap;
